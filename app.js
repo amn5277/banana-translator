cc9f2bacceb99ab17aa05379e851d867310ccdf5
Aman Pareek <amn5277@gmail.com>
2022-06-22 11:55:23 +0530

    functionality added for translation

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ var buttonReference = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
+var translateAPI = "https://api.funtranslations.com/translate/minion.json"
+
+function serverUrlConstructor(text){
+    return translateAPI+ "?"+"text=" + text
+}
+
+function errorHandler(){
+    alert("There is some error on the server end please try after sometime")
+}
+
 function onClickEventHandler(){
-    outputDiv.innerText = "dkfjaslkdf " + textInput.value
+    var inputText  = textInput.value
+   fetch(serverUrlConstructor(inputText))
+   .then(resopnse => resopnse.json())
+   .then(json => {
+        outputDiv.innerText = json.contents.translated
+    })
+    .catch(errorHandler)
 }
 
 buttonReference.addEventListener("click",onClickEventHandler)
